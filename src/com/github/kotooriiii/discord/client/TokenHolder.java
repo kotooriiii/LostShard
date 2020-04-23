d8b9a03a5d82ce34d9803b6b0e219600ef9410cf
Kotori <8682236+KotoriXIII@users.noreply.github.com>
2020-04-23 10:13:01 -0700

    All projects are finished. However, needs debugging check before deploy to live server.

--- src/com/github/kotooriiii/discord/client/TokenHolder.java
+++ src/com/github/kotooriiii/discord/client/TokenHolder.java
@@ -1,5 +1,5 @@
 package com.github.kotooriiii.discord.client;
 
 public class TokenHolder {
-    public static String DISCORD_TOKEN = "~~~Discord token has been hidden~~~";
+    public static String DISCORD_TOKEN = "~~~~Discord token has been hidden~~~~";
 }
